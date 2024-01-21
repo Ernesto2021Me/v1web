1af29b96e83ed5b3d37c59218cbf495bd8fba18e
Ernesto2021Me <153482630+Ernesto2021Me@users.noreply.github.com>
2024-01-21 11:02:11 -0600

    Update keys.js

--- ProyectoWeb/server/src/build/keys.js
+++ ProyectoWeb/server/src/build/keys.js
@@ -2,9 +2,9 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = {
     database: {
-        host: 'localhost',
-        user: 'root',
-        password: '',
+        host: 'database-1-2.cp2yzz2qk8jx.us-east-2.rds.amazonaws.com',
+        user: 'admin',
+        password: 'Admin123',
         database: 'BibliotecaVirtual'
     }
 };
